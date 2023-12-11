32d9d00047657dad67b1756eb86d0a782a7d5120
Sick the Cat <129430216+sickthecat@users.noreply.github.com>
2023-12-11 18:03:08 -0500

    Update SimpleAesEncryption.cs
    40~ mins

--- SimpleAesEncryption.cs
+++ SimpleAesEncryption.cs
@@ -1,1 +1,59 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+public class SimpleAesEncryption
+{
+    private readonly AesManaged _aesManaged;
+
+    public SimpleAesEncryption(byte[] key, byte[] iv)
+    {
+        _aesManaged = new AesManaged
+        {
+            Key = key,
+            IV = iv
+        };
+    }
+
+    public string EncryptString(string plainText)
+    {
+        byte[] encrypted;
+        ICryptoTransform encryptor = _aesManaged.CreateEncryptor(_aesManaged.Key, _aesManaged.IV);
+
+        using (MemoryStream msEncrypt = new MemoryStream())
+        {
+            using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+            {
+                using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                {
+                    swEncrypt.Write(plainText);
+                }
+                encrypted = msEncrypt.ToArray();
+            }
+        }
+
+        return Convert.ToBase64String(encrypted);
+    }
+
+    public string DecryptString(string cipherText)
+    {
+        string plaintext = null;
+        byte[] cipherBytes = Convert.FromBase64String(cipherText);
+        ICryptoTransform decryptor = _aesManaged.CreateDecryptor(_aesManaged.Key, _aesManaged.IV);
+
+        using (MemoryStream msDecrypt = new MemoryStream(cipherBytes))
+        {
+            using (CryptoStream csDecrypt = a new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+            {
+                using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                {
+                    plaintext = srDecrypt.ReadToEnd();
+                }
+            }
+        }
+
+        return plaintext;
+    }
+}
 
